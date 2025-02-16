128328c4c83a336d18b87772f5e85fb4c6c908a6
MichaelGerrardStanleyH <hutagalung.michael23@gmail.com>
2025-02-16 17:27:46 +0700

    update routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/(:num)', 'Home::getById/$1');
-$routes->post('/', 'Home::addDynamicProduct/$1');
-$routes->put('/(:num)', 'Home::editDynamicProduct/$1');
-$routes->delete('/(:num)', 'Home::deleteDynamicProduct/$1');
+
+// rest-api
+$routes->get('/products', 'Home::index');
+$routes->get('/products/(:num)', 'Home::getById/$1');
+$routes->post('/dynamic-products', 'Home::addDynamicProduct/$1');
+$routes->put('/dynamic-products/(:num)', 'Home::editDynamicProduct/$1');
+$routes->delete('/dynamic-products/(:num)', 'Home::deleteDynamicProduct/$1');
